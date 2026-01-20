de62d30f2b94d906c81b153ccdf29bb891bb590d
utsav1103 <utsavu858@gmail.com>
2026-01-20 23:17:33 +0530

    adding frontend url for auth

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -7,4 +7,7 @@ export const ENV = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
     NODE_ENV: process.env.NODE_ENV,
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 }
