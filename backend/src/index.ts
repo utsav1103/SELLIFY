ee21ad9dc1643a49aa619706b4d7a743d7fee4e3
utsav1103 <utsavu858@gmail.com>
2026-01-20 23:17:10 +0530

    adding cors and clerk middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import { ENV } from './config/env';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
+app.use(cors({origin:ENV.FRONTEND_URL})); //enable cors for frontend url
+app.use(clerkMiddleware()); //auth obj wil be attached to the req
+app.use(express.json()); //pareses json body
+app.use(express.urlencoded({ extended: true }));// parses form data
+
 app.get("/", (req, res) => {
+    
     res.json({ message:"Welcome to SELLIFY API - Powered by PostgreSQL,Drizzle ORM & Clerk Auth",
         endpoints:{
             users:"/api/users",
