88ebd8cc58d309498470d91d4620d7e0892166da
utsav1103 <utsavu858@gmail.com>
2026-01-23 14:32:46 +0530

    feat: set up API routes for users, products, and comments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import { ENV } from './config/env';
 import { clerkMiddleware } from '@clerk/express'
+import userRoutes from './routes/userRoutes';
+import productRoutes from './routes/productRoutes';
+import commentRoutes from './routes/commentRoutes';
 
 const app = express();
 
@@ -19,6 +22,10 @@ app.get("/", (req, res) => {
             Comments:"/api/comments",
         },
      });
-})
+});
+
+app.use("/api/users",userRoutes);
+app.use("/api/products",productRoutes );
+app.use("/api/comments", commentRoutes );
 
 app.listen(ENV.PORT, () => console.log("Server is running on port:",ENV.PORT));
