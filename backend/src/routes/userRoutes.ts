292d63fd5c14b2ff2694add3bec61018a630ae12
utsav1103 <utsavu858@gmail.com>
2026-01-23 14:49:15 +0530

    feat: add sync user route with authentication

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
+import { syncUser } from "../controllers/userController";
+import { requireAuth } from "@clerk/express";
 
 const router = Router();
 
+
+// /api/users/sync - Sync users from Clerk to the database
+
+router.post("/sync", requireAuth(), syncUser)
 
 export default router;
