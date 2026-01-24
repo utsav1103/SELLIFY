cbfa00e02f303984ee437e9aa89ed17979c0f5f9
utsav1103 <utsavu858@gmail.com>
2026-01-24 23:41:49 +0530

    feat: add react-router dependency and update App component formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 
 
 
-function App() {  return (
+
+function App() {
+  return (
     <div >
       <h1 className="text-purple-600">Welcome</h1>
       <button className="btn btn-primary">hit me</button>
+      
+
     </div>
   );
-}export default App;
+} export default App;
