4def54073c54ccf571bc085444106302de73817a
utsav1103 <utsavu858@gmail.com>
2026-01-24 23:30:51 +0530

    testing tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@
 
 function App() {  return (
     <div >
-      <h1 className="text-purple-600">Welcome to the App!</h1>
+      <h1 className="text-purple-600">Welcome</h1>
+      <button className="btn btn-primary">hit me</button>
     </div>
   );
 }export default App;
