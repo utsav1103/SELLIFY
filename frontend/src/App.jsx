1d73712e0a8fa1cd4c74a1e9ad9619802672354e
utsav1103 <utsavu858@gmail.com>
2026-01-26 20:24:44 +0530

    feat: add ProductPage, ProfilePage, CreatePage, and EditPage components with routing support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import ProductPage  from "./pages/ProductPage";
+import  ProfilePage  from "./pages/ProfilePage";
+import  CreatePage  from "./pages/CreatePage";
+import  EditPage  from "./pages/EditPage";
 
 
 
@@ -11,6 +15,10 @@ function App() {
       <main className="max-w-5xl mx-auto px-4 py-8">
         <Routes>
           <Route path="/"  element={<HomePage />} />
+          <Route path="/product/:id"  element={<ProductPage />} />
+          <Route path="/profile"  element={<ProfilePage />} />
+          <Route path="/create"  element={<CreatePage />} />
+          <Route path="/edit/:id"  element={<EditPage />} />
         </Routes>
       </main>
      
