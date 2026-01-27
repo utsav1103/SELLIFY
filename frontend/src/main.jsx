24c761d8a8a8214f636cfe747a15fffdedd04394
utsav1103 <utsavu858@gmail.com>
2026-01-27 23:12:51 +0530

    feat: add axios and @tanstack/react-query dependencies, integrate QueryClientProvider in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router';
 
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -12,11 +14,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
+//create a clinet
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
         <App />
+      </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
