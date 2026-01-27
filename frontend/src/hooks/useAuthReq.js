d8eecdad03f7ba957122b779e94ee5a136f0e2cf
utsav1103 <utsavu858@gmail.com>
2026-01-27 23:55:14 +0530

    feat: implement useAuthReq hook for handling authentication requests with token management

---
+++ frontend/src/hooks/useAuthReq.js
@@ -0,0 +1,33 @@
+import { useAuth } from "@clerk/clerk-react"
+import { useEffect } from "react"
+import api from "../lib/axios"
+
+
+function useAuthReq() {
+
+    const { getToken, isLoaded, isSignedIn } = useAuth();
+
+    useEffect(() => {
+        //inslcude the token to teh request
+        const interceptor = api.interceptors.request.use(
+            async (config) => {
+                if (isSignedIn) {
+                    const token = getToken();
+                    if (token) {
+                        config.headers.Authorization = `Bearer ${token}`;
+                    }
+                }
+                return config;
+            });
+        return () => {
+            api.interceptors.request.eject(interceptor);
+        }
+ }, [getToken, isLoaded, isSignedIn]);
+
+    return {
+        isSignedIn,
+        isClerkLoaded:isLoaded
+    };
+}
+
+export default useAuthReq
