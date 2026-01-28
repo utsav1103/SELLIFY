cb9b356bc55680ca568ca8f8346482fbd2af588b
utsav1103 <utsavu858@gmail.com>
2026-01-28 22:27:24 +0530

    feat: create useCreateProduct mutation hook

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,9 +1,14 @@
-import { useQuery } from '@tanstack/react-query';
-import { getAllProducts } from '../lib/api';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { createProduct, getAllProducts } from '../lib/api';
 
 
 export const useProducts = () => {
     const result = useQuery({ queryKey: ['products'], queryFn:getAllProducts});
     return result;
 
+};
+
+export const useCreateProduct = () => {
+    const result = useMutation({mutationFn:createProduct})
+    return result;
 };
