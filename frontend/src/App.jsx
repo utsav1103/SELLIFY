c45932247ef15f6bb134d1d2145707a9799add73
utsav1103 <utsavu858@gmail.com>
2026-01-29 23:01:51 +0530

    feat- adding protection on routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
-import ProductPage  from "./pages/ProductPage";
-import  ProfilePage  from "./pages/ProfilePage";
-import  CreatePage  from "./pages/CreatePage";
-import  EditPage  from "./pages/EditPage";
+import ProductPage from "./pages/ProductPage";
+import ProfilePage from "./pages/ProfilePage";
+import CreatePage from "./pages/CreatePage";
+import EditPage from "./pages/EditPage";
 import useAuthReq from "./hooks/useAuthReq";
 import useUserSync from "./hooks/useUserSync";
 // import { useQuery } from "@tanstack/react-query";
@@ -13,10 +13,10 @@ import useUserSync from "./hooks/useUserSync";
 
 function App() {
   // const {data,isError,isLoading,refetch} = useQuery();
-  const {isClerkLoaded, isSignedIn} = useAuthReq();
+  const { isClerkLoaded, isSignedIn } = useAuthReq();
   useUserSync();
 
-  if(!isClerkLoaded){
+  if (!isClerkLoaded) {
     return null;
   }
 
@@ -26,14 +26,14 @@ function App() {
       <Navbar />
       <main className="max-w-5xl mx-auto px-4 py-8">
         <Routes>
-          <Route path="/"  element={<HomePage />} />
-          <Route path="/product/:id"  element={<ProductPage />} />
-          <Route path="/profile"  element={<ProfilePage />} />
-          <Route path="/create"  element={isSignedIn?<CreatePage />: <Navigate to={"/"}/>} />
-          <Route path="/edit/:id"  element={<EditPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/profile" element={isSignedIn ? <ProfilePage /> : <Navigate to={"/"} />} />
+          <Route path="/create" element={isSignedIn ? <CreatePage /> : <Navigate to={"/"} />} />
+          <Route path="/edit/:id" element={isSignedIn ? <EditPage /> : <Navigate to={"/"} />} />
         </Routes>
       </main>
-     
+
     </div>
   );
 } export default App;
