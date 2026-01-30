915cbe397a3d773d633f5ea081d136dfe06fc700
utsav1103 <utsavu858@gmail.com>
2026-01-30 14:17:07 +0530

    feat- creating useProduct Hook

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,14 +1,23 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { createProduct, getAllProducts } from '../lib/api';
+import { createProduct, getAllProducts, getProductById } from '../lib/api';
 
 
 export const useProducts = () => {
-    const result = useQuery({ queryKey: ['products'], queryFn:getAllProducts});
+    const result = useQuery({ queryKey: ['products'], queryFn: getAllProducts });
     return result;
 
 };
 
 export const useCreateProduct = () => {
-    const result = useMutation({mutationFn:createProduct})
+    const result = useMutation({ mutationFn: createProduct })
     return result;
+};
+
+export const useProduct = (id) => {
+    return useQuery({
+        queryKey: ["product", id],
+        queryFn: () => getProductById(id),
+        enabled: !!id
+        // !! two exclamiantion marks convets to boolean valuse as id is a string we conveted it- !! this is called double bang operator
+    });
 };
