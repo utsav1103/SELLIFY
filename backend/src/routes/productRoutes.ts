98fb1c8004d71de86bb916fd9420d17f28011975
utsav1103 <utsavu858@gmail.com>
2026-01-30 20:11:38 +0530

    feat: fixing replacing /my route to correct place

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -5,12 +5,12 @@ import { requireAuth } from "@clerk/express";
 
 const router = Router();
 
+//get /api/products/my-products - Get products by user id - protected route
+router.get("/my", requireAuth(), productController.getMyProducts);
 //get /api/products/ - Get all products
 router.get("/",productController.getAllProducts);
 //get /api/products/:id - Get product by id
 router.get("/:id", productController.getProductById);
-//get /api/products/my-products - Get products by user id - protected route
-router.get("/my", requireAuth(), productController.getMyProducts);
 //post /api/products/ - Create new product - protected route
 router.post("/", requireAuth(), productController.createProduct);
 //put /api/products/:id - Update product - protected route
