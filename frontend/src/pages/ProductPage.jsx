f45de70482bf33e02876c8977ea5e3e6a2cc4a40
utsav1103 <utsavu858@gmail.com>
2026-01-30 14:57:11 +0530

    feat: adding edit and delete product with check if owner or not

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import {ArrowLeftIcon, EditIcon,Trash2Icon,CalendarIcon,UserIcon} from "lucide-react";
+import LoadingSpinner from "../components/LoadingSpinner";
+//import CommentsSection from "../components/CommentsSection";
+import { useAuth } from "@clerk/clerk-react";
+import { useProduct,useDeleteProduct } from "../hooks/useProducts";
+import {useParams,Link ,useNavigate} from "react-router";
+import { useId } from "react";
+
+
 
 function ProductPage() {
+
+  const {id} = useParams();
+  const {userId} = useAuth();
+  const navigate = useNavigate();
+
+  const {data: product, isLoading , error} = useProduct(id);
+  const deleteProduct = useDeleteProduct();
+
+  const handleDelete = () =>{
+    if(confirm("Delete this product...?")){
+      deleteProduct.mutate(id, {onSuccess:()=> navigate ("/")})
+    }
+  }
+
+  if(isLoading) return <LoadingSpinner/>;
+
+  if(error || !product){
+    return(
+      <div className="card bg-base-300 max-w-md mx-auto">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title text-error">Product not found...
+          </h2>
+            <Link to="/" className="btn btn-primary btn-sm">To Home
+            </Link>
+        </div>
+      </div>
+    );
+  };
+
+  const isOwner = userId === product.userId;
+
+
   return (
-    <div>ProductPage</div>
-  )
+    <div className="max-w-4xl mx-auto space-y-6">
+      <div className="flex items-center justify-between">
+        <Link to="/" className="btn btn-ghost btn-sm gap-1">
+        <ArrowLeftIcon className="size-4"/> Back
+        </Link>
+
+        {isOwner && (
+          <div className="flex gap-2">
+            <Link to={`/edit${product.id}`} className="btn btn-ghost btn-sm gap-1">
+            <EditIcon className="size-4"/> Edit
+            </Link>
+            <button onClick={handleDelete}
+            className="btn btn-error btn-sm gap-1"
+            disabled={deleteProduct.isPending}>
+              {deleteProduct.isPending ?(
+                <span className="loading loading-spinner loading-xs"/>
+              ):(
+                <Trash2Icon className="size-4"/>
+              )} Delete
+            </button>
+            </div>
+        )}
+
+      </div>
+
+        <div className="grid lg:grid-cols-2 gap-6">
+          {/*Image*/}
+          <div className="card bg-base-300">
+            <figure className="p-4">
+              <img src={product.imageUrl}
+              alt={product.title}
+              className="rounded-xl w-full h-80 object-cover"
+              />
+            </figure>
+          </div>
+
+
+
+        </div>
+
+    </div>
+  );
 }
 
-export default ProductPage
+export default ProductPage;
